26a910fb3620c7d3b6dc0f2f31b0c10c1affc15f
nohupped_arch <nohupped@users.noreply.github.com>
2020-07-12 22:28:18 +0800

    use flag

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"log"
 	"os"
 	"traceroute/lib"
+	"flag"
 )
 
 func main() {
+	host := flag.String("host", "127.0.0.1", "hostname/ip to traceroute to")
 	l := log.New(os.Stdout, "", log.Llongfile|log.Ldate)
 	fmt.Println("Starting")
-	c, e := lib.TraceRoute("yahoo.com")
+	c, e := lib.TraceRoute(*host)
 	select {
 	case r := <-c:
 		l.Println(r)
